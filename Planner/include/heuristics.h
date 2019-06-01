c67c78c9647dcc049ddd2254c7387fa7f20e65bb
Adamantios <manoszaras94@gmail.com>
2019-06-01 20:48:40 +0300

    Add method to initialize delta values and delta_max method's skeleton.

--- Planner/include/heuristics.h
+++ Planner/include/heuristics.h
@@ -1,9 +1,27 @@
 #ifndef PDDL_SOLVER_HEURISTICS_H
 #define PDDL_SOLVER_HEURISTICS_H
 
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <problem.hh>
+#include "predicate.hh"
+#include "action.hh"
 
-class heuristics {
+// Predicates mapped with their costs.
+typedef std::unordered_map<Predicate *, int> DeltaMap;
 
+class Heuristics {
+private:
+    LiteralList *_goal;
+
+public:
+    explicit Heuristics(LiteralList *goal);
+
+    DeltaMap init_delta_values(LiteralList *current_state);
+
+    std::unordered_map<Predicate *, int> delta_max(LiteralList *current_state);
 };
 
 
--- Planner/src/heuristics.cpp
+++ Planner/src/heuristics.cpp
@@ -1,1 +1,48 @@
-#include "../include/heuristics.h"
+#include "heuristics.h"
+
+Heuristics::Heuristics(LiteralList *goal) : _goal(goal) {}
+
+DeltaMap Heuristics::init_delta_values(LiteralList *current_state) {
+    DeltaMap delta_map;
+
+    // Iterate through state literals.
+    for (Literal *state_literal : *current_state) {
+        // Get current state_literal's predicate and logical part.
+        Predicate *state_predicate = state_literal->first;
+        bool state_logical_part = state_literal->second;
+        std::cout << "Current: " << *state_literal->first << std::endl;
+
+        // Create iterator, to define if any of the goal's literals matches the state_literal.
+        auto iterator = find_if(_goal->begin(), _goal->end(),
+                                [&](const Literal *literal) -> bool {
+                                    // No need to compare types. Two identical PDDL objects cannot have different types.
+                                    return literal->first->_name == state_predicate->_name &&
+                                           *literal->first->_args == *state_predicate->_args &&
+                                           literal->second == state_logical_part;
+                                });
+
+        // Insert zero if goal exists at the current state, otherwise infinity.
+        delta_map.insert({state_predicate, (iterator != _goal->end() ? 0 : std::numeric_limits<double>::infinity())});
+    }
+
+    return delta_map;
+}
+
+DeltaMap Heuristics::delta_max(LiteralList *current_state) {
+    // Initialize Delta values.
+    DeltaMap delta_map = init_delta_values(current_state);
+
+    // // Define a relaxed set and a flag which marks changes in it.
+    // LiteralList relaxed_set;
+    // bool set_updated;
+    //
+    // // Loop until U does not change.
+    // do {
+    //     // Get applicable actions.
+    //     std::vector<Action *> applicable_actions = parserController.ApplicableActions(current_state);
+    //     // Update flag.
+    //     set_updated = condition ? true : false;
+    // } while (set_updated);
+
+    return delta_map;
+}
