5e58851b703b40dca8707fa81229c0de2e159dff
Efi Kalti <efikalti@gmail.com>
2019-06-05 17:47:25 +0300

    added NextState function

--- Planner/src/ParserController.cpp
+++ Planner/src/ParserController.cpp
@@ -32,7 +32,7 @@ void ParserController::PrintPredicates() {
     vector<Predicate *> *predicates = driver->domain->getPredicates();
     for (auto it = predicates->begin(); it != predicates->end(); ++it)
         cout << *(*it) << endl;
-    cout<<endl;
+    cout << endl;
 }
 
 /**
@@ -60,7 +60,7 @@ void ParserController::PrintAction(Action action) {
             cout << *it << " ";
         cout << action.getPrecond()->at(i)->second << endl;
     }
-    cout<<endl;
+    cout << endl;
 }
 
 /**
@@ -118,7 +118,7 @@ LiteralList *ParserController::GetGoal() {
  *
  * @return bool true if the action can be applied to the state, false otherwise
  */
-vector<vector<string>> ParserController::IsApplicable(LiteralList* state, Action* action) {
+vector<vector<string>> ParserController::IsApplicable(LiteralList *state, Action *action) {
 
     vector<vector<string>> parameterList;
 
@@ -279,7 +279,7 @@ vector<vector<string>> ParserController::IsApplicable(LiteralList* state, Action
 
         if (applicable) {
             vector<string> params;
-            for(unsigned int lvl = 0; lvl < actionParams.size(); lvl++) {
+            for (unsigned int lvl = 0; lvl < actionParams.size(); lvl++) {
                 params.push_back(actionParams.at(lvl).second);
             }
             parameterList.push_back(params);
@@ -313,12 +313,12 @@ vector<vector<string>> ParserController::IsApplicable(LiteralList* state, Action
  *
  * @return vector containing pairs of applicable actions and their applicable params values on the state supplied
  */
-vector<pair<Action*, vector<vector<string>>>> ParserController::ApplicableActions(LiteralList* state) {
+vector<pair<Action *, vector<vector<string>>>> ParserController::ApplicableActions(LiteralList *state) {
 
-    vector<Action*> actions = this->GetActions();
-    vector<pair<Action*, vector<vector<string>>>> applicableActions;
+    vector<Action *> actions = this->GetActions();
+    vector<pair<Action *, vector<vector<string>>>> applicableActions;
     for (unsigned int i = 0; i < actions.size(); i++) {
-        pair<Action*, vector<vector<string>>> applicableActionsParams;
+        pair<Action *, vector<vector<string>>> applicableActionsParams;
         applicableActionsParams.first = actions.at(i);
         applicableActionsParams.second = this->IsApplicable(state, actions.at(i));
         if (!applicableActionsParams.second.empty()) applicableActions.push_back(applicableActionsParams);
@@ -330,11 +330,135 @@ vector<pair<Action*, vector<vector<string>>>> ParserController::ApplicableAction
  *
  * @return the new state after the action is applied to the provided state
  */
-LiteralList* ParserController::NextState(LiteralList *state, Action action, vector<string> param_values) {
+LiteralList *ParserController::NextState(LiteralList *state, Action action, vector<string> param_values) {
+
+    Predicate *state_predicate;
+    Predicate *effect_predicate;
+    Predicate *precondition_predicate;
+    bool effect_status;
+    bool state_predicate_status;
+
+    LiteralList *new_state = new LiteralList();
+    for(unsigned int i=0; i<state->size(); i++) {
+        Literal* literal = new Literal();
+        literal->second = state->at(i)->second;
+        literal->first = state->at(i)->first;
+        new_state->emplace_back(literal);
+    }
+
+    // Keep action parameters on a local HashMap
+    map<string, string> action_params;
+    for (unsigned int i = 0; i < action.getParams()->size(); i++) {
+        // Create HashMap entry with key the param name, and value its value
+        action_params[action.getParams()->at(i)] = param_values.at(i);
+    }
+
+    // Keep precondition parameters on a local HashMap
+    map<string, string> precond_params;
+    for (unsigned int precond_index = 0; precond_index < action.getPrecond()->size(); precond_index++) {
+        precondition_predicate = action.getPrecond()->at(precond_index)->first;
+
+        // Search in state for the predicate that satisfies this precondition
+        for (unsigned int state_index = 0; state_index < new_state->size(); state_index++) {
+            Predicate *state_predicate = new_state->at(state_index)->first;
+            // Compare precondition and state predicates names
+            if (!precondition_predicate->getName().compare(state_predicate->getName()))
+                // If their names are the same, check their bool values
+                if (action.getPrecond()->at(precond_index)->second == new_state->at(state_index)->second) {
+                    // Assign to each parameter the value of the state predicate
+                    for (unsigned int index = 0; index < precondition_predicate->getArgs()->size(); index++) {
+                        precond_params[precondition_predicate->getArgs()->at(index)] = state_predicate->getArgs()->at(
+                                index);
+                    }
+                }
+        }
+    }
+
+    bool applied;
+    unsigned int state_size = state->size();
+    // For each action effect, check if it appears in the state predicates
+    for (unsigned int effects_index = 0;
+         effects_index < action.getEffects()->size(); effects_index++) { // Loop through action's effects
+        // Assign current effect predicate value and status to the local variables
+        effect_predicate = action.getEffects()->at(effects_index)->first;
+        effect_status = action.getEffects()->at(effects_index)->second;
+
+        // Set applied to false as this effect has not been applied yet
+        applied = false;
+
+        // Find all state predicates that appear in the action's effect
+        unsigned int state_index = 0;
+        while (state_index < state_size && !applied) {
+            // Assign current state predicate value to state_predicate
+            state_predicate = state->at(state_index)->first;
+            state_predicate_status = state->at(state_index)->second;
+
+            // Compare effect name with state predicate name
+            if (!effect_predicate->getName().compare(state_predicate->getName())) // returns 0 when equal
+            {
+                if (state_predicate_status != effect_status) {
+                    // Different status between this state predicate and the effect
+
+                    // Check if the parameters match
+                    unsigned int correct_args = 0;
+                    for (unsigned int arg_index = 0; arg_index < state_predicate->getArgs()->size(); arg_index++) {
+                        string param = effect_predicate->getArgs()->at(arg_index);
+
+                        if (!precond_params[param].compare(state_predicate->getArgs()->at(arg_index))) {
+                            // State predicate has the correct value for this parameter, increment counter
+                            correct_args++;
+                        }
+                    }
 
 
+                    // Check if all the state predicate arguments match the effects arguments
+                    if (correct_args == state_predicate->getArgs()->size()) {
+                        // Apply effect
+                        new_state->at(state_index)->second = action.getEffects()->at(effects_index)->second;
 
-    return state;
+                        // Flag applied
+                        applied = true;
+                    }
+                }
+            }
+            state_index++;
+        }
+
+        // Check if the effect was applied in this state
+        if (!applied) {
+            // The effect adds a new literal to the state
+            Literal *new_literal = new Literal();
+            new_literal->first = effect_predicate;
+            for (unsigned int i = 0; i < effect_predicate->getArgs()->size(); i++) {
+                for (map<string, string>::iterator action = action_params.begin();
+                     action != action_params.end(); ++action) {
+
+                    if (!effect_predicate->getArgs()->at(i).compare(action->first))
+                        new_literal->first->getArgs()->at(i) = action->second;
+                }
+            }
+            new_literal->second = effect_status;
+            new_state->emplace_back(new_literal);
+            //delete (new_literal);
+        }
+    }
+
+    return new_state;
+}
+
+vector<LiteralList *> ParserController::NextStates(LiteralList *state, Action action, vector<vector<string>> param_values) {
+
+    vector<LiteralList *> states;// = vector<LiteralList *>();
+    LiteralList * new_state;
+
+    for(unsigned int i=0; i<param_values.size(); i++) {
+        states = vector<LiteralList *>();
+        LiteralList * new_state = NextState(state, action, param_values.at(i));
+        states.emplace_back(new_state);
+        //PrintState(*new_state);
+    }
+
+    return states;
 }
 
 
