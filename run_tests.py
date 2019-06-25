e0a6ed8417eb376c0dd6e1faca1eb35ab01b68ec
Manos Kirtas <manolis.kirt@gmail.com>
2019-06-25 03:11:37 +0300

    Fixes to python script

--- run_tests.py
+++ run_tests.py
@@ -8,17 +8,17 @@ name = [name for name in os.walk("./data/problems/") ]
 
 for domain in domains:
     files = [x[2] for x in os.walk("./data/problems/{}".format(domain))]
-    problems[domain] = []
     for file in files:
-        problems[domain].append(file[0])
-
-# algorithms = ['A_STAR', 'GDFS', 'IDA_STAR', 'DFS', 'BEST_FS' ]
+        problems[domain] = file
+print(problems)
+algorithms = ['A_STAR', 'GDFS', 'IDA_STAR', 'DFS', 'BEST_FS' ]
 algorithms = ['A_STAR']
 
 heuristics = ['MAX_COST', 'ADD_COST']
 
 for domain in problems:
     for problem in problems[domain]:
+        print("Problem", problem)
         for algorithm in algorithms:
             for heuristic in heuristics:
                 cmd = "./Planner/pddl_solver --domain data/domains/{}.pddl --problem data/problems/{}/{} -a {} --heuristic {}".format(domain, domain, problem, algorithm, heuristic)
